305f659b643d5633b63fb92362e895e0546c845b
Diego Sebastian Herrera <diherrer@everis.com>
2020-05-30 12:00:50 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'render2';
+  @ViewChild('contenedorPadre')
+  private contenedorPadre: ElementRef;
+
+  @ViewChild('identificador')
+  private identificador: ElementRef;
+  
+  constructor(private rendered: Renderer2) {     
+  }
+
+  onClickEvent1() {
+    this.rendered.setAttribute(
+      this.contenedorPadre.nativeElement, "miatributo", "mi valor");
+    this.rendered.setAttribute(
+      this.identificador.nativeElement, "value", "Hola mundo");
+
+  }
+
+  onClickEvent2() {
+     this.rendered.removeAttribute(
+       this.contenedorPadre.nativeElement, "miatributo");
+     this.rendered.removeAttribute(
+       this.identificador.nativeElement, "value");
+    
+  }
+
+  onClickEvent3() {
+    this.rendered.setProperty(this.identificador.nativeElement,"value","desde propiedad");
+    this.rendered.setProperty(this.identificador.nativeElement,"disabled","true");
+   
+ }
+
 }
